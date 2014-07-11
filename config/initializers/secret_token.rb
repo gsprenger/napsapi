12a9f9bbe0a5c8cf03906979e408606117efcf34
Gabriel Sprenger <gabriel.sprenger@gmail.com>
2014-07-11 17:46:03 +0200

    Reinstalled Rails with version 4.0.8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Napsapi::Application.config.secret_key_base = '3acd3b697d0a6c208cd7ce87f0e9a038ef11535d81e9def3bc960f2b856cab700d452bc9c237113b94131c81f4e619bc195171e7306e463a765a94366bd232cd'
