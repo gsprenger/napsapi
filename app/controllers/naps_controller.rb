75809e2d9f7f55519971b45b12b9c83da3ba744f
Gabriel Sprenger <gabriel.sprenger@gmail.com>
2014-07-11 19:55:49 +0200

    Testing different CSRF parameters

--- app/controllers/naps_controller.rb
+++ app/controllers/naps_controller.rb
@@ -1,5 +1,5 @@
 class NapsController < ApplicationController
-  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+  protect_from_forgery with: :null_session
   before_action :set_nap, only: [:show, :edit, :update, :destroy]
 
   # GET /naps
